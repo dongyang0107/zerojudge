0f1cadf10e0be6a6e94547aabc3b730ac7863b01
chendongyang <110306024@nccu.edu.tw>
2023-07-26 13:43:46 +0800

    一元二次方程式

--- a006.py
+++ a006.py
@@ -3,7 +3,7 @@
 while True:
 	a, b, c = map(int, input().split())
 	if b*b-4*a*c >= 0:
-		# 在Python中，變數宣告是不需要的，只需要對變數進行賦值即可
+		# 在Python中，變數宣告是不需要的，只需對變數進行賦值即可
 		# 會導致出現SyntaxError: invalid syntax錯誤
 		# int x1
 		# int x2
