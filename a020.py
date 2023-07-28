35df89face238c81d34df5ac8ad4966550fefe2f
chendongyang <110306024@nccu.edu.tw>
2023-07-28 16:05:19 +0800

    身分證驗證機

---
+++ a020.py
@@ -0,0 +1,17 @@
+# 身分證驗證機
+
+dic = {'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15, 
+'G':16, 'H':17, 'I':34, 'J':18, 'K':19, 'L':20, 'M':21,
+'N':22, 'O':35, 'P':23, 'Q':24, 'R':25, 'S':26, 'T':27, 
+'U':28, 'V':29, 'W':32, 'X':30, 'Y':31, 'Z':33}
+user = input()
+word = dic[user[0]]
+num = word//10
+num1 = word%10
+total_num = num1*9 + num + int(user[9])
+for i in range(1, 9):
+	total_num = total_num + int(user[i])*(9-i)
+if total_num%10 == 0:
+	print('real')
+else:
+	print('fake')
